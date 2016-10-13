248f810435b48e7ed8ed0a3e6a3261ce679ce8f8
Rubén Chavarría <txingo@gmail.com>
2016-10-13 22:04:21 +0200

    Add new `estate` component
    Estate is meant to be the property of the owners, houses, homes,
    flats, etc that will be visited by potential customers.

---
+++ src/app/estate/estate.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { EstateComponent } from './estate.component';
+
+describe('Component: Estate', () => {
+  it('should create an instance', () => {
+    let component = new EstateComponent();
+    expect(component).toBeTruthy();
+  });
+});
