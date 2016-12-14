ad91ae2adc869c2e7b87541a15daa51e29f3fed3
Rubén Chavarría <txingo@gmail.com>
2016-12-14 23:23:30 +0100

    Add some tests for SignatureComponent

--- src/app/signature/signature.component.spec.ts
+++ src/app/signature/signature.component.spec.ts
@@ -1,11 +1,38 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async } from '@angular/core/testing';
+import { ElementRef } from '@angular/core';
 import { SignatureComponent } from './signature.component';
 
+import * as SignaturePad from 'signature_pad';
+
 describe('Component: Signature', () => {
+
+  function buildComponentHTMLElements() {
+    const div = document.createElement('div');
+    const canvas = document.createElement('canvas');
+    div.appendChild(canvas);
+
+    return new ElementRef(div);
+  }
+
   it('should create an instance', () => {
     let component = new SignatureComponent();
     expect(component).toBeTruthy();
   });
+
+  it('there is no pad by default', () => {
+    const component = new SignatureComponent(null);
+    expect(component.pad).toBeUndefined();
+  });
+
+  it('creates a pad from an element on init', () => {
+    const el = buildComponentHTMLElements();
+    const component = new SignatureComponent(el);
+
+    component.ngOnInit();
+
+    expect(component.pad).toBeDefined();
+  });
+
 });
