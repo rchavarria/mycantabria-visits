6149d4a683630de804e439f1ade0ac6d9b05936f
Rubén Chavarría <txingo@gmail.com>
2017-01-19 22:50:59 +0100

    Start using the model to hold and show information about the `Estate`

--- src/app/estate/estate.component.ts
+++ src/app/estate/estate.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Address } from '../header/address.model';
+import { Amount } from './amount.model';
+import { Owner } from './owner.model';
+import { Estate } from './estate.model';
+
 @Component({
   selector: 'estate',
   templateUrl: './estate.component.html',
   styleUrls: ['./estate.component.css']
 })
 export class EstateComponent implements OnInit {
+  estate: Estate;
 
   constructor() { }
 
   ngOnInit() {
+    const address: Address = new Address(
+      'Calle Dalí, 8',
+      19162,
+      'Pioz',
+      'Cantabria'
+    );
+
+    const amount = new Amount(400, '€');
+
+    const owner = new Owner('Juana la loca');
+
+    this.estate = new Estate(
+      '12344321',
+      address,
+      amount,
+      owner
+    );
   }
 
 }
