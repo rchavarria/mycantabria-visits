f892c1a184ce19fa99a3957fff12e5fb2b7bae74
Rubén Chavarría <txingo@gmail.com>
2017-01-19 22:39:53 +0100

    Add model to store information about how much does it cost to rent/buy a estate

---
+++ src/app/estate/amount.model.ts
@@ -0,0 +1,14 @@
+
+export class Amount {
+  quantity: number;
+  currency: string;
+
+  constructor(
+    quantity: number,
+    currency: string
+  ) {
+    this.quantity = quantity;
+    this.currency = currency;
+  }
+  
+}
