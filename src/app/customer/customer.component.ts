fc3ca1ddb6a9075f20f0bf5760786f55843a7400
Rubén Chavarría <txingo@gmail.com>
2017-01-22 22:31:38 +0100

    Use `Customer` model to show information about her in the page

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Customer } from './customer.model';
+
 @Component({
   selector: 'customer',
   templateUrl: './customer.component.html',
   styleUrls: ['./customer.component.css']
 })
 export class CustomerComponent implements OnInit {
+  customer: Customer;
 
   constructor() { }
 
   ngOnInit() {
+    this.customer = new Customer(
+      '2719',
+      'Sheila',
+      '12344321234',
+      'sheilaher1981@hotmail.com'
+    );
   }
 
 }
