f1cf8dc047e37d8ff240e4260aa4f284af7fa862
Rubén Chavarría <txingo@gmail.com>
2017-01-22 22:38:26 +0100

    Add and use a model class to hold the Warning message

--- src/app/warning/warning.component.ts
+++ src/app/warning/warning.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Warning } from './warning.model';
+
 @Component({
   selector: 'warning',
   templateUrl: './warning.component.html',
   styleUrls: ['./warning.component.css']
 })
 export class WarningComponent implements OnInit {
+  warning: Warning;
 
   constructor() { }
 
   ngOnInit() {
+    this.warning = new Warning();
   }
 
 }
---
+++ src/app/warning/warning.model.ts
@@ -0,0 +1,17 @@
+
+export class Warning {
+  message: string;
+
+  constructor() {
+    this.message = `
+Asimismo el posible arrendatario/comprador manifiesta que se compromete a no
+realizar ninguna gestión encaminada a adquirir por sí mismo, por medio de
+apoderado o por conducto de terceras personas el inmueble visitado por
+mediación de MyCantabria Inmobiliaria, ni a poner la información de la que
+dispone a disposición de alguno de sus parientes en línea directa o colateral,
+para que sean ellos los que realicen el alquiler/compra de dicho inmueble a
+espaldas de MyCantabria Inmobiliaria.
+    `.trim();
+  }
+}
+
