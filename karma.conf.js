58a931b7795a9bc1012f64fb1ebbd91616277dcc
Rubén Chavarría <txingo@gmail.com>
2017-01-24 22:02:35 +0100

    Use Firefox to run tests

--- karma.conf.js
+++ karma.conf.js
@@ -7,7 +7,7 @@ module.exports = function (config) {
     frameworks: ['jasmine', 'angular-cli'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-chrome-launcher'),
+      require('karma-firefox-launcher'),
       require('karma-remap-istanbul'),
       require('angular-cli/plugins/karma')
     ],
